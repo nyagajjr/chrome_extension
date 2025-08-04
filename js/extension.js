7810beb9c333095379f598db09035ddd193e8e05
nyagajjr <nyagaben123@gmail.com>
2025-08-04 16:42:59 +0200

    Adding inputs from the input field into the array

--- js/extension.js
+++ js/extension.js
@@ -1,6 +1,11 @@
-let btn = document.querySelector('#input-btn')
 
-btn.addEventListener("click", function(){
-    console.log("Neu teknique")
+let elems = []
+let btn = document.querySelector('#input-btn')
+let inputEl = document.querySelector("#input-el")
+
+btn.addEventListener("click", () => {
+    elems.push(inputEl.value)
+    console.log(elems)
 })
+
 

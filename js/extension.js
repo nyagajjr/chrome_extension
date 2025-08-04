3b284337ba2236319e319153384004fa6d1dc876
nyagajjr <nyagaben123@gmail.com>
2025-08-04 22:35:20 +0200

    Js refactoring

--- js/extension.js
+++ js/extension.js
@@ -2,10 +2,19 @@
 let elems = []
 let btn = document.querySelector('#input-btn')
 let inputEl = document.querySelector("#input-el")
+let ulEl = document.querySelector("#ul-el")
 
 btn.addEventListener("click", () => {
     elems.push(inputEl.value)
-    console.log(elems)
+    listThem()
 })
 
+
+function listThem(){
+    let items = ""
+    for (let i = 0; i < elems.length; i++){
+        items += "<li>" + elems[i] + "</li>"
+    }
+    ulEl.innerHTML = items
+}
 

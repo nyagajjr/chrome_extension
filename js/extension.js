735f022e358ee1be242d4b2a38366e6aa99c7cbf
nyagajjr <nyagaben123@gmail.com>
2025-08-07 15:31:05 +0200

    Adding link of the value

--- js/extension.js
+++ js/extension.js
@@ -3,6 +3,7 @@ let elems = []
 let btn = document.querySelector('#input-btn')
 let inputEl = document.querySelector("#input-el")
 let ulEl = document.querySelector("#ul-el")
+let anchorEl = document.querySelector("#anchor-el")
 
 btn.addEventListener("click", () => {
     elems.push(inputEl.value)
@@ -14,12 +15,16 @@ btn.addEventListener("click", () => {
 function listThem(){
     let items = ""
     for (let i = 0; i < elems.length; i++){
-        items += "<li>" + elems[i] + "</li>"
+        items += "<a>" + elems[i] + "</a>"
     }
-    ulEl.innerHTML = items
+    anchorEl.innerHTML = items
 }
 
 //Clearing the field after hitting the buy button
 function clearField(){
     document.querySelector("#input-el").value = ""
 }
+
+anchorEl.addEventListener("click", ()=>{
+    window.open("youtube.com")
+})
